13789a01ba6f57baa8378fdabd265687af79eae1
Diego Barrios Romero <eldruin@gmail.com>
2018-10-24 22:50:29 +0200

    Add linux example

---
+++ examples/linux.rs
@@ -0,0 +1,13 @@
+extern crate embedded_hal;
+extern crate linux_embedded_hal;
+extern crate tmp1x2;
+
+use linux_embedded_hal::I2cdev;
+use tmp1x2::{ Tmp1x2, SlaveAddr };
+
+fn main() {
+    let dev = I2cdev::new("/dev/i2c-1").unwrap();
+    let mut sensor = Tmp1x2::new(dev, SlaveAddr::default());
+    let temperature = sensor.read_temperature().unwrap();
+    println!("Temperature: {}", temperature);
+}
