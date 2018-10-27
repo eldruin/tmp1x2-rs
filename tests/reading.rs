4017c274e7eb3037655363fc16172ba066a17cfe
Diego Barrios Romero <eldruin@gmail.com>
2018-10-27 13:22:57 +0200

    Refactor reading tests

--- tests/reading.rs
+++ tests/reading.rs
@@ -13,29 +13,22 @@ fn get_expectation(register: u8, lsb: u8, msb: u8) -> [I2cTransaction; 1] {
     ]
 }
 
-#[test]
-fn can_read_temperature() {
-    let expectations = get_expectation(Register::TEMPERATURE, 0, 0b0110_0100);
-    let mut dev = setup(&expectations);
-    let temp = dev.read_temperature().unwrap();
-    assert_eq!(100.0, temp);
-    dev.destroy().done();
+macro_rules! read_test {
+    ($name:ident, $method:ident, $register:ident, $lsb:expr, $msb:expr, $expected:expr) => {
+        #[test]
+        fn $name() {
+            let expectations = get_expectation(Register::$register, $lsb, $msb);
+            let mut dev = setup(&expectations);
+            let value = dev.$method().unwrap();
+            assert_eq!($expected, value);
+            dev.destroy().done();
+        }
+    };
 }
 
-#[test]
-fn can_one_shot_measurement_is_not_ready() {
-    let expectations = get_expectation(Register::CONFIG, DEFAULT_CONFIG_LSB, DEFAULT_CONFIG_MSB);
-    let mut dev = setup(&expectations);
-    let is_ready = dev.is_one_shot_measurement_result_ready().unwrap();
-    assert!(!is_ready);
-    dev.destroy().done();
-}
+read_test!(can_read_temperature, read_temperature, TEMPERATURE, 0, 0b0110_0100, 100.0);
 
-#[test]
-fn can_one_shot_measurement_is_ready() {
-    let expectations = get_expectation(Register::CONFIG, DEFAULT_CONFIG_LSB | BFL::ONE_SHOT, DEFAULT_CONFIG_MSB);
-    let mut dev = setup(&expectations);
-    let is_ready = dev.is_one_shot_measurement_result_ready().unwrap();
-    assert!(is_ready);
-    dev.destroy().done();
-}
+read_test!(one_shot_result_not_ready, is_one_shot_measurement_result_ready, CONFIG,
+           DEFAULT_CONFIG_LSB,                 DEFAULT_CONFIG_MSB, false);
+read_test!(one_shot_result_ready,     is_one_shot_measurement_result_ready, CONFIG,
+           DEFAULT_CONFIG_LSB | BFL::ONE_SHOT, DEFAULT_CONFIG_MSB, true);
